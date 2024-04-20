d82e50459dc89b65d92f13e99d9fdd2a18ed8404
Harsith R <96562342+ILikeHerculePoirot@users.noreply.github.com>
2024-04-20 14:52:46 +0530

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,39 +1,43 @@
-import pygame as g,random as r,time,sys
-c=g.display.set_mode((800,600))
-g.display.set_caption('Snake')
+import pygame
+import random
+import time
+import sys
+screen=pygame.display.set_mode((800,600))
+pygame.display.set_caption('Snake')
 while True:
-    p=r.randint(1,780)
-    q=r.randint(1,580)
-    f=g.Rect(p-(p%20),q-(q%20),20,20)
-    s=[[0,0]]
-    l=''
+    food_x=random.randint(1,39)
+    food_y=random.randint(1,29)
+    food=pygame.Rect(food_x*20,food_y*20,20,20)
+    snake_coordinates=[[0,0]]
+    direction=''
     while True:
-        for e in g.event.get():
-            if e.type==g.QUIT:sys.exit()
-            if(e.type==g.KEYDOWN):
-                if(e.key==g.K_w and l!='d'):l='u'
-                elif(e.key==g.K_s and l!='u'):l='d'
-                elif(e.key==g.K_a and l!='r'):l='l'
-                elif(e.key==g.K_d and l!='l'):l='r'
-        a=[s[0][0],s[0][1]]
-        if(l=='u'):s[0][1]-=20
-        elif(l=='d'):s[0][1]+=20
-        elif(l=='l'):s[0][0]-=20
-        elif(l=='r'):s[0][0]+=20
-        if(s[0][0]<0 or s[0][0]>800 or s[0][1]<0 or s[0][1]>600 or len(s)!=len(set(tuple(b) for b in s))):break
-        if(s[0][0]==f.x and s[0][1]==f.y):
-            while True:
-                p=r.randint(1,780)
-                q=r.randint(1,580)
-                f.topleft=(p-(p%20),q-(q%20))
-                if(s.count([p-(p%20),q-(q%20)])==0):break
-            if(l=='u'):s.append([s[-1][0],s[-1][1]+20])
-            elif(l=='d'):s.append([s[-1][0],s[-1][1]-20])
-            elif(l=='l'):s.append([s[-1][0]+20,s[-1][1]])
-            elif(l=='r'):s.append([s[-1][0]-20,s[-1][1]])
-        for i in range(1,len(s)):h=s[i];s[i]=a;a=h
-        c.fill((0,0,0))
-        g.draw.rect(c,(255,255,255),f)
-        for p in s:g.draw.rect(c,(255,255,255),g.Rect(p[0],p[1],20,20))
+        for event in pygame.event.get():
+            if(event.type==pygame.QUIT):sys.exit()
+            if(event.type==pygame.KEYDOWN):
+                if((event.key==pygame.K_w or event.key==pygame.K_UP) and direction!='down'):direction='up'
+                elif((event.key==pygame.K_s or event.key==pygame.K_DOWN) and direction!='up'):direction='down'
+                elif((event.key==pygame.K_a or event.key==pygame.K_LEFT) and direction!='right'):direction='left'
+                elif((event.key==pygame.K_d or event.key==pygame.K_RIGHT) and direction!='left'):direction='right'
+        previous_coordinate=[snake_coordinates[0][0],snake_coordinates[0][1]]
+        if(direction=='up'):snake_coordinates[0][1]-=20
+        elif(direction=='down'):snake_coordinates[0][1]+=20
+        elif(direction=='left'):snake_coordinates[0][0]-=20
+        elif(direction=='right'):snake_coordinates[0][0]+=20
+        if(snake_coordinates[0][0]<0 or snake_coordinates[0][0]>800 or snake_coordinates[0][1]<0 or snake_coordinates[0][1]>600 or len(snake_coordinates)!=len(set(tuple(coordinate) for coordinate in snake_coordinates))):break
+        if(snake_coordinates[0][0]==food.x and snake_coordinates[0][1]==food.y):
+            food_x=random.randint(1,39)
+            food_y=random.randint(1,29)
+            food.topleft=(food_x*20,food_y*20)
+            if(direction=='up'):snake_coordinates.append([snake_coordinates[-1][0],snake_coordinates[-1][1]+20])
+            elif(direction=='down'):snake_coordinates.append([snake_coordinates[-1][0],snake_coordinates[-1][1]-20])
+            elif(direction=='left'):snake_coordinates.append([snake_coordinates[-1][0]+20,snake_coordinates[-1][1]])
+            elif(direction=='right'):snake_coordinates.append([snake_coordinates[-1][0]-20,snake_coordinates[-1][1]])
+        for i in range(1,len(snake_coordinates)):
+            current_coordinate=snake_coordinates[i]
+            snake_coordinates[i]=previous_coordinate
+            previous_coordinate=current_coordinate
+        screen.fill((0,0,0))
+        pygame.draw.rect(screen,(255,255,255),food)
+        for coordinate in snake_coordinates:pygame.draw.rect(screen,(255,255,255),pygame.Rect(coordinate[0],coordinate[1],20,20))
         time.sleep(0.05)
-        g.display.update()
+        pygame.display.update()
